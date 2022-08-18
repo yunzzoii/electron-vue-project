65e4a6126cd7ec647d0e5531b2a252ba6e7f06c8
Minae-Kim <kimminae3654@naver.com>
2022-08-18 22:20:17 +0900

    Update ScriptResult.vue

--- vue-electron-test/src/components/home/ScriptResult.vue
+++ vue-electron-test/src/components/home/ScriptResult.vue
@@ -1,7 +1,20 @@
 <!-- ScriptResult.vue -->
 <template>
-  <div>
-    변환 결과 자리입니다
+  <div class="rwd_box___1ysJh">
+    <div class="translate_area___3xdxa translate_result___21ZBH mo_show___GaMYQ">
+      <div class="edit_area___2iv-G" style="display: none;">
+        <textarea class="edit_box___1KtZ3 active___3VPGL" maxlength="50000" rows="1" style="resize: none; height: 27px;"></textarea>
+        <div class="btn_toolbar___20tub">
+          <span class="tooltip___3KoCI">
+            <span class="tooltipbox___1ArNp">
+              <div class="tip___27k1n" style="left: -38px; right: -38px; width: 76px;">
+                <span>코드복사하기</span>
+              </div>
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
